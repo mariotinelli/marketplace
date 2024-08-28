81719e4508e84129dd40263962edc6bf272c1880
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-08-27 21:32:50 -0300

    MKT-34: fill store factory

--- database/factories/StoreFactory.php
+++ database/factories/StoreFactory.php
@@ -4,22 +4,21 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Enums\StoreType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Store>
- */
 class StoreFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'type'    => $this->faker->randomElement(StoreType::cases()),
+            'name'    => $this->faker->company(),
+            'phone'   => $this->faker->numerify('###########'),
+            'address' => $this->faker->streetAddress(),
+            'city'    => $this->faker->city(),
+            'state'   => $this->faker->stateAbbr(),
+            'zipcode' => $this->faker->postcode(),
         ];
     }
 }
