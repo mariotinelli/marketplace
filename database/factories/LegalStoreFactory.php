1d742d3e73204ba436725926be0f22a38f43ff48
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-08-27 21:51:22 -0300

    MKT-34: format file

--- database/factories/LegalStoreFactory.php
+++ database/factories/LegalStoreFactory.php
@@ -12,11 +12,11 @@ class LegalStoreFactory extends Factory
     public function definition(): array
     {
         return [
-            'store_id' => Store::factory(),
-            'cnpj' => $this->faker->numerify('##############'),
-            'name' => $this->faker->company(),
-            'corporate_name' => $this->faker->company(),
-            'state_registration' => $this->faker->numerify('########'),
+            'store_id'               => Store::factory(),
+            'cnpj'                   => $this->faker->numerify('##############'),
+            'name'                   => $this->faker->company(),
+            'corporate_name'         => $this->faker->company(),
+            'state_registration'     => $this->faker->numerify('########'),
             'municipal_registration' => $this->faker->numerify('########'),
         ];
     }
