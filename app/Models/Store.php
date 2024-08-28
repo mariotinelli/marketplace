28cfaaae246096bc44d0fbd18b706d343af7e5e5
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-08-27 21:30:39 -0300

    MKT-34: add fillable

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -10,4 +10,22 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'name',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'type'  => StoreType::class,
+            'phone' => ConvertPhone::class,
+        ];
+    }
 }
