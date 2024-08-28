b91c4b76e4ab0fc61b9e992c76dace330c4eaade
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-08-27 21:24:44 -0300

    MKT-34: fill store migration

--- database/migrations/2024_08_28_002137_create_stores_table.php
+++ database/migrations/2024_08_28_002137_create_stores_table.php
@@ -7,20 +7,22 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('type');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('stores');
