c05d7aed77e9b60e242bbcf33da5cf4b88580608
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-08-27 21:47:04 -0300

    MKT-34: insert columns on migration

--- database/migrations/2024_08_28_004422_create_legal_stores_table.php
+++ database/migrations/2024_08_28_004422_create_legal_stores_table.php
@@ -7,20 +7,20 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('legal_stores', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->string('cnpj')->unique();
+            $table->string('name');
+            $table->string('corporate_name');
+            $table->string('state_registration');
+            $table->string('municipal_registration');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('legal_stores');
