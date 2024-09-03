270fbe8f2964c8794e9c9514758a45db2ba6e4a5
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:30:07 -0300

    MKT-34: insert columns on migration

--- database/migrations/2024_09_03_012813_create_products_table.php
+++ database/migrations/2024_09_03_012813_create_products_table.php
@@ -7,20 +7,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->foreignId('brand_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description', 600);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
