d78058becd422fe76561b44ffca24f3173a762d3
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:45:07 -0300

    MKT-34: insert columns on factory

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -4,22 +4,16 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Models\Sku;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
- */
 class ImageFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'sku_id' => Sku::factory(),
+            'url'    => $this->faker->imageUrl(),
         ];
     }
 }
