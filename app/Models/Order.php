7e30f55fcba64d0ba9f039ea3027d12d4a180468
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:50:37 -0300

    MKT-34: insert columns on fillable

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,4 +10,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'buyer_id',
+        'status',
+        'total',
+    ];
 }
