1530ce7d9601eaaff1917ebc66428004fda83009
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:34:40 -0300

    MKT-34: insert columns on factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,22 +4,22 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Store;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
- */
 class ProductFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'store_id'    => Store::factory(),
+            'brand_id'    => Brand::factory(),
+            'category_id' => Category::factory(),
+            'name'        => $name = $this->faker->sentence(2),
+            'slug'        => str($name)->slug()->toString(),
+            'description' => $this->faker->paragraph(),
         ];
     }
 }
