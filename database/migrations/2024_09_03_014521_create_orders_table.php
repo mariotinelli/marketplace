470459fabce9977bd34d2980c264d69462fad280
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:47:08 -0300

    MKT-34: insert columns on migration

--- database/migrations/2024_09_03_014521_create_orders_table.php
+++ database/migrations/2024_09_03_014521_create_orders_table.php
@@ -7,20 +7,17 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('buyer_id')->constrained();
+            $table->tinyInteger('status');
+            $table->bigInteger('total');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('orders');
