16f1eaf1c0043976c19006a4cc3e95d6a49b65ef
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:25:40 -0300

    MKT-34: insert columns on factory

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -6,20 +6,12 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
- */
 class BrandFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->sentence(2),
         ];
     }
 }
