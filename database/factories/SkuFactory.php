d9ecab8f55bb90566a9b9b4fc2979c7ae1731694
MARIO HENRIQUE RODRIGUES TINELLI <49689215+mariotinelli@users.noreply.github.com>
2024-09-02 22:40:42 -0300

    MKT-34: insert columns on factory

--- database/factories/SkuFactory.php
+++ database/factories/SkuFactory.php
@@ -4,22 +4,18 @@ declare(strict_types = 1);
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sku>
- */
 class SkuFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'product_id' => Product::factory(),
+            'name'       => $this->faker->sentence(2),
+            'price'      => $this->faker->randomNumber(5),
+            'quantity'   => $this->faker->numberBetween(1, 100),
         ];
     }
 }
